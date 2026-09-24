c30db99cf505de88b31d1dbda442a5955cc85019
biplabijubaksangha-dot <biplabijubaksangha@gmail.com>
2026-09-24 23:32:21 +0530

    Update Photo

--- committee.js
+++ committee.js
@@ -189,7 +189,7 @@ const committeeMembers = [
         name: "Ananda Kumar Mohapatra",
         fatherName: "Narendra Mohapatra",
         designation: "Member",
-        photo: "assets/images/committee-01.jpg",
+        photo: "assets/images/members/member10.jpg",
         registrationNo: "BJS-16508-001",
         bloodGroup: "B-",
         dateOfJoining: "01-01-2023",
